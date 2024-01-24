0fb1cce24b755397b782c4857a7b199790318b7d
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2024-01-24 17:15:29 -0300

    unfinished manage expense page

--- App.js
+++ App.js
@@ -64,13 +64,22 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: "white",
+          }}
+        >
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpenseOverview}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+          <Stack.Screen
+            name="ManageExpense"
+            component={ManageExpense}
+            options={{ presentation: "modal" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,50 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { View, StyleSheet } from "react-native";
 
-function ManageExpense() {
-  return <Text>ManageExpense Screen</Text>;
+import IconButton from "../components/UI/iconButton";
+import { GlobalStyles } from "../constants/styles";
+
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditting = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditting ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditting]);
+
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.container}>
+      {isEditting && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
