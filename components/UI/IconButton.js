a2ed4a29dabbe19025bdd7e8d7e75a9490389b32
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2024-01-24 16:39:55 -0300

    add icon button

--- components/UI/IconButton.js
+++ components/UI/IconButton.js
@@ -1,4 +1,4 @@
-import { Pressable, View, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 function IconButton({ icon, size, color, onPress }) {
